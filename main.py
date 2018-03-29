328fc93db9d10e91741b80671561261e744aef4e
Nick Gilliland <gillilandnicholas1@gmail.com>
2018-03-28 21:04:25 -0500

    Cant get it in the text box

--- main.py
+++ main.py
@@ -9,19 +9,18 @@ form = """
 <html>
     <head>
         <style>
-             form {
-                 background-color: #eee;
-                 padding: 20px;
-                 margin: 0 auto;
-                 width: 540px;
-                 font: 16px sans-serif;
-                 border-radius: 10px;
-             }
-             textarea {
-                 margin: 10px 0;
-                 width: 540px;
-                 heigth: 120px;
-             }
+            form {background-color: red;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
         </style>
     </head>
     <body>
@@ -31,28 +30,31 @@ form = """
                 Rotate by:
                 <input type="text" name="rot" value="0" />
             </lable>
-            <input type="text" name="message" />
+            <br>
+            <textarea type="text" name="message">{0}</textarea>
+            <br>
             <input type="submit"/>
         </form>
     </body>
 </html>
 """
-
+#<input type="text" name="message" />
 @app.route("/", methods=['POST'])
 def encrypt():
     rotation = request.form['rot']
     rotation = int(rotation)
     message = request.form['message']
 
+    #global encrypted_text
     encrypted_text = rotate_string(message,rotation)
     
-    return "<h1>" + encrypted_text + "</h1>"
+    return form.format(encrypted_text)
 
 
 @app.route("/")
 def index():
-    content = form
     
-    return content
+    encrypted_text = ''
+    return form.format(encrypted_text)
 
 app.run()
